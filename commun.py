57d3887181ef48529bfc56b5c8c43031dd152dc3
Marc Petrus <153208018+MarcPetrus@users.noreply.github.com>
2023-12-12 19:37:31 +0100

    Update commun.py
    Chargement de df (source ORDE) par csv en trois parties.

--- commun.py
+++ commun.py
@@ -11,15 +11,18 @@ title="Projet Énergie"
 
 @st.cache_data
 def getDf():
-    return pd.read_csv("sources_donnees/eco2mix-prepare-temperatures_indispo_prix.csv", sep=";", parse_dates=['datetime'], index_col=0)
+    return pd.read_csv(datadir+"eco2mix-prepare-temperatures_indispo_prix.csv", sep=";", parse_dates=['datetime'], index_col=0)
 
 @st.cache_data
 def getDfx():
-    #return pd.read_csv(datadir+'eco2mix-regional-cons-def.csv', sep=";", index_col=0)
-    return pd.read_csv("https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-regional-cons-def/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B", sep=";")
-
+    #return pd.read_csv("https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-regional-cons-def/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B", sep=";")
+    df1 = pd.read_csv(datadir+'eco2mix-regional-cons-def_part1.csv')
+    df2 = pd.read_csv(datadir+'eco2mix-regional-cons-def_part2.csv')
+    df3 = pd.read_csv(datadir+'eco2mix-regional-cons-def_part3.csv')
+    df_tot = pd.concat([df1, df2,df3], ignore_index=True).drop(columns=['Unnamed: 0'])
+    return df_tot
+    
 df = getDf()
 dfx = getDfx()
-
 
 liste_regions = (df['region'].unique())
